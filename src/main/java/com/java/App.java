61a38328ab19c8540236167340ca59fdb06025fd
Ahmed Mahfouz <amahfouz50@outlook.com>
2023-05-28 14:26:31 +0300

    Update App.java

--- src/main/java/com/java/App.java
+++ src/main/java/com/java/App.java
@@ -8,11 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.err.println( "Hello World!" );
         String whereCluse = """
 				semester = %d
 				and name = %s
 				""".formatted(441 , "'ahmed'");
-System.out.println(whereCluse );
+System.err.println(whereCluse );
     }
 }
